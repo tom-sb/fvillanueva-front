cb432c0d6e4e440b337ce30f9060f12460fdcb0e
tom-sb <fvillanuevanut@gmail.com>
2025-03-29 23:43:06 -0500

    feat: content width

--- src/components/pages/news/news-view.tsx
+++ src/components/pages/news/news-view.tsx
@@ -10,23 +10,57 @@ import {
   Typography,
 } from '@mui/material';
 import FooterPage from '../../organims/footer-page';
+import React from 'react';
 
 export default function NewsView() {
+
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
+  
+  const card = (
+    <React.Fragment>
+      <CardContent>
+        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </React.Fragment>
+  );
+
   return (
     <Container
       maxWidth={false}
       disableGutters
       sx={{
-        bgcolor: "red", 
+        bgcolor: "green", 
         flexGrow: 1,
         display: "flex",
         flexDirection: "column",
         // El 100vh es opcional, si quieres que ocupe todo el alto
         minHeight: "100vh",
+        alignItems:'center'
       }}
     >
       <Box>
-        <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{ maxWidth: '100vh' }}>
           <CardActionArea>
             <CardMedia
               component="img"
@@ -50,6 +84,9 @@ export default function NewsView() {
             </Button>
           </CardActions>
         </Card>
+      </Box>
+      <Box sx={{ minWidth: '100vh' }}>
+        <Card variant="outlined">{card}</Card>
       </Box>
       <FooterPage />
     </Container>
